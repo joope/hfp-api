f84e9bc49e67ac74d3d6656b5323d6e9ddcc8392
joope <pyry.joona@helsinki.fi>
2019-08-23 20:16:54 +0300

    Add vehicle tracking

--- tracking.js
+++ tracking.js
@@ -3,8 +3,9 @@
 
 const AWS = require('aws-sdk');
 const mqtt = require('mqtt');
-const sqs = new AWS.SQS({region : 'eu-west-1'});
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const sns = new AWS.SNS({region : 'eu-west-1'});
+
 
 function prefixWithZeroes(input, desiredLength) {
   const diff = Math.max(desiredLength - input.length, 0);
@@ -25,11 +26,10 @@ module.exports.start = async (event) => {
   }
 
   try {
-    const res = await sns.publish({
+    await sns.publish({
       Message: JSON.stringify(data),
       TopicArn: process.env.SNS_ARN,
     }).promise();
-    console.log(res);
     return {
       statusCode: 200,
       body: "Started tracking the vehicle"
@@ -45,7 +45,7 @@ module.exports.start = async (event) => {
 
 module.exports.track = (event) => {
   return new Promise(async (resolve, reject) => {
-    const worker = setTimeout(() => {
+    setTimeout(() => {
       resolve({
         statusCode: 200,
         body: 'Finished tracking',
@@ -58,25 +58,6 @@ module.exports.track = (event) => {
       operator_id, 
       vehicle_number 
     } = data;
-
-    const targetQueue = `${transport_mode}${operator_id}${vehicle_number}`;
-
-    const params = {
-      QueueName: targetQueue,
-      Attributes: {
-        MessageRetentionPeriod: "120",
-        VisibilityTimeout: "120"
-      }
-    }
-
-    let QueueUrl = '';
-    try {
-      const response = await sqs.createQueue(params).promise();
-      console.log(response);
-      QueueUrl = response.QueueUrl;
-    } catch (e) {
-      console.error(e);
-    }
 
     const topic = `/hfp/v1/journey/ongoing/${transport_mode}/${prefixWithZeroes(operator_id, 4)}/${prefixWithZeroes(vehicle_number, 5)}/+/+/+/+/+/+/#`;
 
@@ -95,23 +76,18 @@ module.exports.track = (event) => {
         return;
       }
 
-      const route = vehicle_position.desi;
-      //vehicles are identified with combination of operator id and vehicle id
-      const vehicle = vehicle_position.oper + "/" + vehicle_position.veh;
-      
-      const position = vehicle_position.lat + "," + vehicle_position.long;
-      const speed = vehicle_position.spd;
+      const params = {
+        TableName: process.env.DYNAMODB_TABLE,
+        Item: {
+          lat: vehicle_position.lat,
+          long: vehicle_position.long,
+          vehicleId: `${transport_mode}${operator_id}${vehicle_number}`,
+          createdAt: new Date().getTime(),
+        },
+      };
 
-      console.log("Route "+route+" (vehicle "+vehicle+"): "+position+" - "+speed+"m/s");
-    
-      // write to sqs
       try {
-        await sqs.sendMessage({
-          QueueUrl,
-          MessageBody: JSON.stringify({
-            ...vehicle_position
-          }),
-        }).promise();
+        dynamoDb.put(params).promise()
       } catch (e) {
         console.error(e);
       }
@@ -123,94 +99,32 @@ module.exports.getStatus = async (event) => {
   const { pathParameters={} } = event;
   const { transport_mode, operator_id, vehicle_number } = pathParameters;
 
-  var params = {
-    AttributeNames: [
-       "SentTimestamp"
-    ],
-    MaxNumberOfMessages: 10,
-    MessageAttributeNames: [
-       "All"
-    ],
-    QueueUrl: `${process.env.SQS_URL}/${transport_mode}${operator_id}${vehicle_number}`,
-    VisibilityTimeout: 20,
-    WaitTimeSeconds: 0
-   };
-   const messages = await sqs.receiveMessage(params).promise();
-  return {
-    statusCode: 200,
-    body: JSON.stringify(messages),
-  };
+  try {
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      KeyConditionExpression: "vehicleId = :vehicle and createdAt > :date",
+      ExpressionAttributeValues: {
+        ":vehicle": `${transport_mode}${operator_id}${vehicle_number}`,
+        ":date": new Date().getTime() - 1000*120
+      }, 
+      Select: "ALL_ATTRIBUTES",
+    };
+    const res = await dynamoDb.query(params).promise();
+    const polyline = res.Items.map(item => ([item.lat, item.long]));
+    const latest = res.Items.pop();
+    const currentPosition = [latest.lat, latest.long]
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        currentPosition,
+        polyline
+      }),
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(e),
+    }
+  }
 }
-
-
-// function getExecutionStatus(params){
-//   return new Promise((res, rej) => {
-//     stepfunctions.describeExecution(params, (err, { status }) => {
-//       if (err) {
-//         rej(err);
-//       }
-//       res(status);
-//     })
-//   })
-// }
-
-// function stopExecution(params){
-//   return new Promise((res, rej) => {
-//     stepfunctions.stopExecution(params, (err, data) => {
-//       if (err) {
-//         console.log(err, err.stack) // an error occurred
-//         return rej(err)
-//       }
-//       console.log(data) // successful response
-//       console.log(data.executionArn) // needed for cancels
-//       console.log(data.startDate)
-//       const response = {
-//         statusCode: 200,
-//         body: JSON.stringify({
-//           message: 'Stopped the vehicle tracking',
-//           params: params
-//         }),
-//       };
-//       res(response);
-//     })
-//   })
-// }
-
-// function startExecution(params){
-//   return new Promise((res, rej) => {
-//     stepfunctions.startExecution(params, (err, data) => {
-//       if (err) {
-//         console.log(err, err.stack) // an error occurred
-//         return rej(err)
-//       }
-//       console.log(data) // successful response
-//       console.log(data.executionArn) // needed for cancels
-//       console.log(data.startDate)
-//       const response = {
-//         statusCode: 200,
-//         body: JSON.stringify({
-//           message: 'Started the vehicle tracking',
-//           params: params
-//         }),
-//       };
-//       res(response);
-//     })
-//   })
-// }
-
-// module.exports.startStepFunction = async () => {
-//   const params = {
-//     name: 'taskName',
-//     stateMachineArn: process.env.STATE_MACHINE_ARN,
-//   }
-
-//   const status = await getExecutionStatus(params);
-
-//   if (status === 'RUNNING') {
-//     await stopExecution(params);
-//   }
-
-//   const response = await startExecution(params);
-
-//   return response;
-// };
